8c6f4923ea8e51ffb77a14f9f35d8b2b8d7ccd56
andrew-h-lee <mr.ahlee@gmail.com>
2016-10-31 15:16:47 +1100

    Add JS Pjt

---
+++ JS-scratch-box/README.md
@@ -0,0 +1,2 @@
+## Reference
+* [JS-Stack from Scratch](https://github.com/verekia/js-stack-from-scratch)
---
+++ JS-scratch-box/src/server/index.js
@@ -0,0 +1,7 @@
+/* eslint-disable no-console */
+
+import Dog from '../shared/dog';
+
+const andre = new Dog('Andre');
+
+console.log(andre.bark());
